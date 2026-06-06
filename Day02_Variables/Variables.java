10bb31e16b5d846de5e36a44251a4d701f627821
Darshan <darshankaware650@gmail.com>
2026-06-06 23:14:32 +0530

    Day 02: Variables Program

---
+++ Day02_Variables/Variables.java
@@ -0,0 +1,16 @@
+public class Variables{
+    public static void main(String[] args) {
+        String name = ("Java Programmer");
+        int age =  19 ;
+        float pointer = 7.45f;
+        char a = 'A';
+        boolean dsk = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(pointer);
+        System.out.println(a);
+        System.out.println(dsk);
+
+    }
+}
