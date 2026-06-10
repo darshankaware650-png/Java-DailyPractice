6e4aae436f58f9f19744a5b3309395a0bc74dfdb
Darshan <darshankaware650@gmail.com>
2026-06-10 19:44:44 +0530

    Day06: Learned Arrays in Java

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+package Day06_Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
