e5bbd186da78447b562ddc2f03b7bfa38cc2394b
Darshan <darshankaware650@gmail.com>
2026-06-10 19:57:37 +0530

    Add Day06 arrays practice

--- Day06_Arrays/Arrays.java
+++ Day06_Arrays/Arrays.java
@@ -4,8 +4,6 @@ public class Arrays {
     public static void main(String[] args) {
         int[] numbers = {10, 20, 30, 40, 50};
 
-        System.out.println("Array elements:");
-
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
