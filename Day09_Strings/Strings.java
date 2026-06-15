df0df829ec57d81ddc47a6b0febe6203077a8e3d
Darshan <darshankaware650@gmail.com>
2026-06-15 21:49:29 +0530

    Learned Strings

---
+++ Day09_Strings/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Darshan";
+
+        System.out.println("Name: " + name);
+        System.out.println("Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+        System.out.println("First character: " + name.charAt(0));
+    }
+}
