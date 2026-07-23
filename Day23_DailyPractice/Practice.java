ff101520ada964f92da209df45f427f58ccc19b8
Darshan Sunil Kaware <darshan.kaware.comp.25@vpkbiet.org>
2026-07-23 22:39:29 +0530

    Array in Java

--- Day23_DailyPractice/Practice.java
+++ Day23_DailyPractice/Practice.java
@@ -1,32 +1,14 @@
-import java.util.Scanner;
-public class Practice{
-    public static void main(String args []){
-        Scanner sc = new Scanner(System.in);
-        String firstName=sc.next();
-        String lastName=sc.next();
-        String fullName = firstName+" "+lastName;
+class A{
+    int x;
+}
+class B{
+    int y;
+    public static void main(String args[]){
+        A ob1 = new A();
+        ob1.x = 8;
+        B ob2 = new B();
+        ob2.y = 8566555;
 
-        System.out.println("FullName is: "+ fullName);
-        // Using Functions
-        // 1st:  .length() to calculate length of string 
-        System.out.println("length of string is: "+fullName.length());
-
-        // 2nd is .charAt:  using for finding the letter at specific index or printing alphabets index wise
-        // using loop for printing all imdex alphabets
-        for(int i=0; i<fullName.length(); i++){
-        System.out.println(fullName.charAt(i));
-        }
-
-        // Now we will create two strings and compare them 
-        // if the string1==string2 then output will be zero
-        // if the string1!=string2 then output will be negative
-        String name1 = "DARSHAN";
-        String name2 = "DARSHAN";
-        if(name1.compareTo(name2)==0){
-            System.out.println("Strings are equal");
-        }
-        else{
-            System.out.println("Strings are not equal");
-        }
+        System.out.println(ob1.x * ob2.y);
     }
 }
